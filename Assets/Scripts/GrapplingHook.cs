56723e82c82604bbc69fd0b71d3399eec436a8ea
Trent <Xmedia101@yahoo.com>
2017-04-18 09:34:31 -0400

    GrapplingHook75%Done
    All that I have left to do is after the hook is instantiated, I have to turn the joint on and connect it, and the line renderer in the same frame

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -2,33 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrapplingHook : MonoBehaviour {
+public class GrapplingHook : MonoBehaviour
+{
 
     [SerializeField]
     private bool IsGrappling = false;
     [SerializeField]
     private bool CanGrapple = true;
     [SerializeField]
-    private Vector2 Rinput;
+    private Vector3 Rinput;
+    [SerializeField]
+    private GameObject Hook;
+    [SerializeField]
+    private float RopeLength;
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        Rinput = new Vector2(Input.GetAxis("RightStickX"), Input.GetAxis("RightStickY"));
+    private float GunCD;
+    private float fireDelay;
+    private Vector3 hookOffset = new Vector3(0, 0.5f, 0);
 
-        if (Rinput.sqrMagnitude < 0.1f)
+    private SpriteRenderer _sr;
+
+    // Use this for initialization
+    void Start()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rinput = new Vector3(Input.GetAxis("RightStickX"), Input.GetAxis("RightStickY"));
+        if (Input.GetButton("GrapplingHook"))
+        {
+            FireGrapple();
+        }
+        if (Rinput.sqrMagnitude < 0.05f)
         {
             return;
         }
         var angle = Mathf.Atan2(Input.GetAxis("RightStickX"), Input.GetAxis("RightStickY")) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(angle, 0, 0);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
 
+
+    }
+
+    void FireGrapple()
+    {
+        if (CanGrapple = true)
+        {
+
+            Debug.Log("Shooting");
+            Vector3 offset = transform.rotation * hookOffset;
+            Instantiate(Hook, transform.position + offset, transform.rotation);
+        }
 
     }
 }
---
+++ Assets/Scripts/HookMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovement : MonoBehaviour {
+
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * 100);
+	}
+	void OnCollision2D(Collision2D col)
+    {
+        rb.isKinematic = true;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
