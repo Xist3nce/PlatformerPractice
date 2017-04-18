0087c5ea76f86740b19f2a3ffc877531eaffbd24
Trent <Xmedia101@yahoo.com>
2017-04-18 04:49:40 -0400

    Fucked it all up.

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -17,7 +17,9 @@ public class PlayerMovementScript : MonoBehaviour
     [SerializeField]
     private float Vinput;
     [SerializeField]
-    private bool InAir;
+    private bool InAir = true;
+    [SerializeField]
+    private bool Grounded = false;
 
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
@@ -45,57 +47,68 @@ public class PlayerMovementScript : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+
         Hinput = Input.GetAxis("Horizontal");
 
-        if (Input.GetButton("Jump"))
-            Debug.Log("TryingToJump");
-            Jump();
- 
-        if (Input.GetButton("Horizontal"))
-            Hinput = 1;
-            _rb.AddForce(transform.right * Hinput * Movespeed * SpeedMult);
 
-        if (Input.GetAxisRaw("Horizontal") != 0 )
+ 
+        //if (Input.GetButton("Horizontal"))
+        //{
+        //    Hinput = 1;
+        //    _rb.AddForce(transform.right * Hinput * Movespeed * SpeedMult);
+        //}
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
             _rb.AddForce(transform.right * Hinput * Movespeed * SpeedMult);
+        }
+
+        //if (Input.GetButton("Jump"))
+        //{
+        //    Debug.Log("TryingToJump");
+        //    Jump();
+        //}
         else;
 
 
         
 	}
 
-    void Jump()
-    {
-        if (InAir == false)
-        {
-            _rb.AddForce(transform.up * JumpForce * JumpMult);
-        }
-        else
-        {
-            Debug.Log("Can't Jump while in air!");
-        }
-    }
+    //void Jump()
+    //{
+    //    if (Grounded == true)
+    //    {
+    //        _rb.AddForce(transform.up * JumpForce * JumpMult);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Can't Jump while in air!");
+    //    }
+    //}
 
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Platform")
-        {
-            JumpCountLeft = JumpCountMax;
-            InAir = false;
-        }
-        else
-        {
-            Debug.Log("Col is Not a platform");
-        }
-    }
-    void OnCollisionExit(Collision col)
-    {
-        if (col.gameObject.tag == "Platform")
-        {
-            InAir = true;
-        }
-        else
-        {
-            Debug.Log("Exitting col with something not a platform");
-        }
-    }
+    //void OnCollisionEnter(Collision col)
+    //{
+    //    if (col.gameObject.tag == "Platform")
+    //    {
+    //        JumpCountLeft = JumpCountMax;
+    //        InAir = false;
+    //        Grounded = true;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Col is Not a platform");
+    //    }
+    //}
+    //// This was my other checker but it's dumb and antiquated.
+    //void OnCollisionExit(Collision col)
+    //{
+    //    if (col.gameObject.tag == "Platform")
+    //    {
+    //        InAir = true;
+    //        Grounded = false;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Exitting col with something not a platform");
+    //    }
+    //}
 }
