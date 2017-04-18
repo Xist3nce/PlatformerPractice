59fe642dee0b6efe758b4ec382cd4174aa7b546c
Trent <Xmedia101@yahoo.com>
2017-04-18 07:19:18 -0400

    Tons of fixes, added a broken dash.

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -20,6 +20,15 @@ public class PlayerMovementScript : MonoBehaviour
     private bool InAir = true;
     [SerializeField]
     private bool Grounded = false;
+    [SerializeField]
+    private bool CanJump = true;
+    [SerializeField]
+    private int LastDirection;
+    [SerializeField]
+    private float DashLength = 3;
+    private float DashCooldown = 3;
+    private bool Dashing = false;
+    private bool CanDash = true;
 
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
@@ -33,7 +42,7 @@ public class PlayerMovementScript : MonoBehaviour
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
@@ -41,17 +50,36 @@ public class PlayerMovementScript : MonoBehaviour
         MaxSpeedMult = 1;
         MaxSpeed = 50;
         JumpMult = 1;
-        
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+        JumpCountLeft = JumpCountMax;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
 
         Hinput = Input.GetAxis("Horizontal");
+        if (Hinput != 0)
+        {
+            if (Hinput > 0)
+            {
+                LastDirection = 1;
+            }
+            else
+            {
+                LastDirection = -1;
+            }
+        }
+
+        if (Input.GetButton("Dash")&&CanDash==true)
+        {
+            Debug.Log("Trying to Dash");
+            Dashing = true;
+            Dash();
+        }
 
 
- 
+
         //if (Input.GetButton("Horizontal"))
         //{
         //    Hinput = 1;
@@ -62,53 +90,71 @@ public class PlayerMovementScript : MonoBehaviour
             _rb.AddForce(transform.right * Hinput * Movespeed * SpeedMult);
         }
 
-        //if (Input.GetButton("Jump"))
-        //{
-        //    Debug.Log("TryingToJump");
-        //    Jump();
-        //}
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("TryingToJump");
+            Jump();
+
+        }
         else;
 
 
-        
-	}
 
-    //void Jump()
-    //{
-    //    if (Grounded == true)
-    //    {
-    //        _rb.AddForce(transform.up * JumpForce * JumpMult);
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Can't Jump while in air!");
-    //    }
-    //}
+    }
 
-    //void OnCollisionEnter(Collision col)
-    //{
-    //    if (col.gameObject.tag == "Platform")
-    //    {
-    //        JumpCountLeft = JumpCountMax;
-    //        InAir = false;
-    //        Grounded = true;
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Col is Not a platform");
-    //    }
-    //}
-    //// This was my other checker but it's dumb and antiquated.
-    //void OnCollisionExit(Collision col)
-    //{
-    //    if (col.gameObject.tag == "Platform")
-    //    {
-    //        InAir = true;
-    //        Grounded = false;
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Exitting col with something not a platform");
-    //    }
-    //}
+    void Dash()
+    {
+            _rb.AddForce(transform.right * Movespeed * 15 * LastDirection); //dash is broken, but I'm keeping this for now because no idea.
+            StartCoroutine(DashCooldownTimer());
+    }
+
+    void Jump()
+    {
+        if (JumpCountLeft > 0 && CanJump == true)
+        {
+            _rb.AddForce(transform.up * JumpForce * JumpMult);
+            JumpCountLeft--;
+            CanJump = false;
+            StartCoroutine(JumpCooldown());
+        }
+        else
+        {
+            Debug.Log("Out of jumps!");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) //This is the platform col
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            JumpCountLeft = JumpCountMax;
+            InAir = false;
+            Grounded = true;
+        }
+        else
+        {
+            Debug.Log("Col is Not a platform");
+        }
+    }
+    // This was my other checker but it's dumb and antiquated.
+    void OnCollisionExit2D(Collision2D col)
+    {
+        InAir = true;
+        Grounded = false;
+
+    }
+    private IEnumerator JumpCooldown()
+    {
+        yield return new WaitForSeconds(0.3f);
+        CanJump = true;
+
+    }
+    private IEnumerator DashCooldownTimer()
+    {
+        yield return new WaitForSeconds(DashLength);
+        Dashing = false;
+        CanDash = false;
+        yield return new WaitForSeconds(DashCooldown);
+        CanDash = true;
+    }
 }
